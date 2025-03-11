69e1d50dd732dd9818993e7397eb1bce88053eed
Jonathan <canojonathanbenavides@gmail.com>
2025-03-11 16:49:27 +0100

    login arreglado

--- proyecto-final-front/src/main.ts
+++ proyecto-final-front/src/main.ts
@@ -1,7 +1,14 @@
+// main.ts (en src/)
+
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { enableProdMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+// Ajusta la ruta: desde src/ hasta src/app/
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [ provideRouter(routes) ]
+})
+.catch(err => console.error(err));
