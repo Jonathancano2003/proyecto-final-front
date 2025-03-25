5944f76067c3761c964200bc94db8841cb51587e
Marc Pla Viusa <st2020108@365.stucom.com>
2025-03-25 15:37:25 +0100

    admin_panel

--- proyecto-final-front/src/app/app.module.ts
+++ proyecto-final-front/src/app/app.module.ts
@@ -5,13 +5,18 @@ import { RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';  
 import { RegisterComponent } from './components/register/register.component';
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';  
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    AdminPanelComponent
+    AdminPanelComponent,
+    
+    ReactiveFormsModule
+
   ],
   imports: [
     BrowserModule,
--- proyecto-final-front/src/app/app.routes.ts
+++ proyecto-final-front/src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { PaginaInicioComponent } from './components/pagina-inicio/pagina-inicio.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
+
+
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'inicio', component: PaginaInicioComponent },
+  { path : 'admin', component: AdminPanelComponent},
+  
   { path: '**', redirectTo: 'login' }
 ];
