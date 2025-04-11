b7e4fdc218bb40e10e465ee9f077d20f119a4a0f
ferranlopez11 <pl2023316@365.stucom.com>
2025-04-11 15:58:30 +0200

    commit 11/4

--- proyecto-final-front/src/app/components/new-ad/new-ad.component.ts
+++ proyecto-final-front/src/app/components/new-ad/new-ad.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-ad',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './new-ad.component.html',
   styleUrl: './new-ad.component.css'
 })
 export class NewAdComponent {
   newAdForm: FormGroup;
+
+  marcas: string[] = [
+    'Abarth', 'Alfa Romeo', 'Audi', 'Aston Martin', 'BMW', 'Bentley', 'Bugatti', 'BYD',
+    'Cadillac', 'Chevrolet', 'Citroën', 'Cupra', 'Dacia', 'Dodge', 
+    'Fiat', 'Ford', 'Honda', 'Hyundai', 'Kia', 'Mazda', 'Mercedes-Benz', 'Nissan', 'Opel', 'Peugeot',
+    'Renault', 'Seat', 'Skoda', 'Toyota', 'Volkswagen', 'Volvo'
+  ];
 
   constructor(private fb: FormBuilder) {
     this.newAdForm = this.fb.group({
