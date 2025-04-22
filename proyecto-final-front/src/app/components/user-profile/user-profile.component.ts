4b6c869b0f1aca1202d6b6399803545aceed7deb
ferranlopez11 <pl2023316@365.stucom.com>
2025-04-22 15:46:42 +0200

    commit 22/4

--- proyecto-final-front/src/app/components/user-profile/user-profile.component.ts
+++ proyecto-final-front/src/app/components/user-profile/user-profile.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-profile',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
   userProfileForm: FormGroup;
+  mostrarFormulario = false;
 
   constructor(private fb: FormBuilder) {
     this.userProfileForm = this.fb.group({
