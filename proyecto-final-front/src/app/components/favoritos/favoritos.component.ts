db4466b550bf10bacd40a22c698064abf822995a
Marc Pla Viusa <st2020108@365.stucom.com>
2025-05-22 18:45:18 +0200

    marc duplicate fav

--- proyecto-final-front/src/app/components/favoritos/favoritos.component.ts
+++ proyecto-final-front/src/app/components/favoritos/favoritos.component.ts
@@ -5,20 +5,22 @@ import { AuthService } from '../../services/auth.service';
 import { CarCardComponent } from '../car-card/car-card.component';
 import { Coche } from '../../models/coche.model';
 import { Router } from '@angular/router';
-import { VehiculosService } from '../../services/vehiculos.service'; // ✅ Importar el servicio
+import { VehiculosService } from '../../services/vehiculos.service'; 
 
 @Component({
   selector: 'app-favoritos',
   standalone: true,
-  imports: [CommonModule, CarCardComponent],
+  imports: [CommonModule],
   templateUrl: './favoritos.component.html',
   styleUrls: ['./favoritos.component.css']
 })
 export class FavoritosComponent implements OnInit {
   favoritos: Coche[] = [];
+  idsFavoritos: Set<number> = new Set();
+
 
   private favoritosService = inject(FavoritosService);
-  private vehiculosService = inject(VehiculosService); // ✅ Inyectar
+  private vehiculosService = inject(VehiculosService); 
   private authService = inject(AuthService);
   private router = inject(Router);
 
@@ -27,13 +29,34 @@ export class FavoritosComponent implements OnInit {
     if (!usuario) return;
 
     this.favoritosService.obtenerFavoritosPorUsuario(usuario.id).subscribe({
-      next: (data) => this.favoritos = data,
+      next: (data) => {
+        this.favoritos = data;
+        this.idsFavoritos = new Set(data.map(c => c.id)); 
+      },
       error: (err) => console.error('Error al cargar favoritos:', err)
     });
   }
 
   verDetalles(car: Coche) {
-    this.vehiculosService.setSelectedCar(car); // ✅ Guardar coche antes de navegar
+    this.vehiculosService.setSelectedCar(car); 
     this.router.navigate(['/coche-select']);
+  }
+  agregarAFavoritos(coche: Coche) {
+    const usuario = this.authService.getUsuarioActual();
+    if (!usuario) return;
+  
+    if (this.idsFavoritos.has(coche.id)) {
+      alert('Este coche ya está en tus favoritos');
+      return;
+    }
+  
+    this.favoritosService.guardarFavorito(usuario.id, coche.id).subscribe({
+      next: () => {
+        this.favoritos.push(coche);          
+        this.idsFavoritos.add(coche.id);     
+        alert('Coche añadido a favoritos');
+      },
+      error: (err) => console.error('Error al guardar favorito:', err)
+    });
   }
 }
